f89bf07d48eafd5fbed4935236d05205c8b0e310
ravihamidov <ravihamidov42@gmail.com>
2025-07-02 02:00:07 +0400

    Add: Task's Structure

---
+++ LabTasks/Task_17-06-25/src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // Java Home Task: "Fitness Tracker Assistant" - Create a small console-based
+        // "Fitness Tracker" Assistant that collects user input, performs calculations
+        // using operators, and prints meaningful feedback.
+
+        // I write here Step-by-Step Requirements:
+        // 1. Collect User Info
+        // - Name (String)
+        // - Age (int)
+        // - Weight in kg (double)
+        // - Height in meters (double)
+        // - Has Gym Membership? (boolean)
+
+    }
+}
