0f3a76db88f600e28ede8924fbdf85a11f5d2096
ravihamidov <ravihamidov42@gmail.com>
2025-07-10 12:06:50 +0400

    Pass: Because i don't understand

---
+++ LanguageLearning/Day_006/Project_002/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("I'm passed printf topic because i don't understand it well");
+    }
+}
