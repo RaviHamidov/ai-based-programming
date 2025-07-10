b6fe27058c25e4c8803e4b8c5d9d990d9bbcad9d
ravihamidov <ravihamidov42@gmail.com>
2025-07-10 12:06:33 +0400

    Pass: Because easy

---
+++ LanguageLearning/Day_006/Project_001/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("I'm passed java Math method topic because soo easy for me to understand");
+    }
+}
