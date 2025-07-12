a2c957ed16de328f8d870e2c272243e5f122c3d1
ravihamidov <ravihamidov42@gmail.com>
2025-07-12 21:08:54 +0400

    Add: Weight Converter App

--- LanguageLearning/Day_008/Project_001/src/App.java
+++ LanguageLearning/Day_008/Project_001/src/App.java
@@ -10,17 +10,22 @@ public class App {
         int choice;
 
         System.out.println("Welcome to the weight converter program!");
-        System.out.print("Enter your weight (with kg): ");
-        weight = scanner.nextDouble();
+      
 
         System.out.println("Choose the conversion type:");
+        System.out.println("1. Convert from Pounds to Kilograms");
+        System.out.println("2. Convert from Kilograms to Pounds");
         choice = scanner.nextInt();
 
         if (choice == 1) {
+            System.out.print("Enter your weight (with lbs): ");
+            weight = scanner.nextDouble();
             // Convert from pounds to kilograms
-            newWeight = weight;
+            newWeight = weight / 2.20462;
             System.out.printf("Your weight in Kilograms is: %.2f kg%n", newWeight);
         } else if (choice == 2) {
+            System.out.print("Enter your weight (with kg): ");
+            weight = scanner.nextDouble();
             // Convert from kilograms to pounds
             newWeight = weight * 2.20462;
             System.out.printf("Your weight in Pounds is: %.2f lbs%n", newWeight);
