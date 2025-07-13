6ceacf6449975efd33fca88ab548c33c12b2d987
ravihamidov <ravihamidov42@gmail.com>
2025-07-13 23:14:53 +0400

    Fix: Some problems

--- LanguageLearning/Day_009/Project_002/src/App.java
+++ LanguageLearning/Day_009/Project_002/src/App.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Simple Calculator");
