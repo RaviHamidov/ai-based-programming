70d85b135ac9957bd341e5ab75bbd316ba536c0f
ravihamidov <ravihamidov42@gmail.com>
2025-07-30 00:29:50 +0400

    GitHub Organize

--- 001_lab-tasks/006_task_11-07-25/001_class-task/book-with-getters-setters/src/App.java
+++ 001_lab-tasks/006_task_11-07-25/001_class-task/book-with-getters-setters/src/App.java
@@ -1,5 +1,15 @@
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+        Book myBook = new Book("Java 101", "John Smith", 120);
+
+        System.out.println("Book: " + myBook.getName());
+
+        myBook.setName("Java Programming Language");
+        myBook.setPageCount(35);
+
+        System.out.println("Updated Book: " + myBook.getName()
+                + ", Pages: " + myBook.getPageCount());
     }
 }
