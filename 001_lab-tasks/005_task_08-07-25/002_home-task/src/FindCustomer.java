31b7ba160312dd6ffc9325b640c5689e7bb78259
ravihamidov <ravihamidov42@gmail.com>
2025-08-12 20:12:37 +0400

    Fix: Task reniwed

--- 001_lab-tasks/005_task_08-07-25/002_home-task/src/FindCustomer.java
+++ 001_lab-tasks/005_task_08-07-25/002_home-task/src/FindCustomer.java
@@ -2,30 +2,30 @@ import java.util.Scanner;
 
 public class FindCustomer {
 
-    static int[] customerIds = { 101, 102, 103, 104, 105 };
+    static String[] customerIds = { "101", "102", "103", "104", "105" };
 
-    public static void findCustomer(int[] customerIds) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Musteri ID-ni daxil edin(): ");
-        int id = scanner.nextInt();
-        boolean found = false;
-
+    public static int findCustomer(String customerId) {
         for (int i = 0; i < customerIds.length; i++) {
-            if (customerIds[i] == id) {
-                System.out.println("Musteri tapildi: " + id);
-                found = true;
-                break;
+            if (customerIds[i].equals(customerId)) {
+                return i;
             }
         }
+        return -1;
+    }
 
-        if (!found) {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Musteri ID-ni daxil edin: ");
+        String id = scanner.nextLine();
+
+        int index = findCustomer(id);
+
+        if (index != -1) {
+            System.out.println("Musteri tapildi. Indeks: " + index);
+        } else {
             System.out.println("Musteri tapilmadi.");
         }
 
         scanner.close();
-    }
-
-    public static void main(String[] args) {
-        findCustomer(customerIds);
     }
 }
